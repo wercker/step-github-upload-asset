de7b0ca79d7b7e72046352da9fac3ea81884f703
Benno van den Berg <bennovandenberg@gmail.com>
2014-05-07 12:09:36 +0200

    Add more info statements

--- run.sh
+++ run.sh
@@ -26,6 +26,8 @@ export_id_to_env_var() {
 
   local id=$(echo "$json" | $WERCKER_STEP_ROOT/bin/jq ".id");
 
+  info "exporting asset id ($id) to environment variable: \$$export_name";
+
   export $export_name=$id;
 }
 
@@ -58,30 +60,35 @@ main() {
   # Set variables to defaults if not set by the user
   if [ -z "$name" ]; then
     name=$(basename "$file")
+    info "no name was supplied; using basename of \$file: $name";
   fi
 
   if [ -z "$owner" ]; then
     owner="$WERCKER_GIT_OWNER";
+    info "no GitHub owner was supplied; using GitHub owner of build repository: $owner";
   fi
 
   if [ -z "$repo" ]; then
     repo="$WERCKER_GIT_REPOSITORY";
+    info "no GitHub repository was supplied; using GitHub repository of build: $repo";
   fi
 
   if [ -z "$content_type" ]; then
     content_type=$(file --mime-type -b "$file");
-    info "no content-type was given, used 'file' to get the content-type: $content_type";
+    info "no content-type was supplied, using 'file' to get the content-type: $content_type";
   fi
 
   if [ -z "$release_id" ]; then
     release_id="$WERCKER_GITHUB_CREATE_RELEASE_ID";
+    info "no release id was supplied, using release id from \$WERCKER_GITHUB_CREATE_RELEASE_ID: $release_id";
   fi
 
   if [ -z "$export_id" ]; then
     export_id="WERCKER_GITHUB_UPLOAD_ASSET_ID";
+    info "no export id was supplied, using default value: $export_id";
   fi
 
-  echo "release_id $release_id";
+  info "starting upload of asset $file to GitHub repo $owner/$repo with release $release_id";
 
   # Upload asset and save the output from curl
   local UPLOAD_RESPONSE=$(upload_asset \
@@ -93,7 +100,11 @@ main() {
     "$file" \
     "$release_id");
 
+  info "finished upload of asset";
+
   export_id_to_env_var "$UPLOAD_RESPONSE" "$export_id";
+
+  info "successfully uploaded asset to GitHub";
 }
 
 # Run the main function
